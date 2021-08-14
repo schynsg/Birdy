f7ad341e07b64d54754cfc8a31dc3d91b0bfa2e3
Goran <goran.schyns@student.hepl.be>
2021-08-14 17:28:28 +0200

    Create all connection components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Header from "../Header";
 import Landing from "../Landing";
+import Welcome from "../Welcome";
+import Login from "../Login";
+import Signin from "../Signin";
+import ErrorPage from "../Errorpage";
 import '../../App.css';
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Landing />
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+
+            <Welcome/>
+            <Login/>
+            <Signin/>
+            <ErrorPage/>
+
+            <Landing/>
+        </div>
+    );
 }
 
 export default App;
