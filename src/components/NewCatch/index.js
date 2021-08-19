6a00ba84acea156840e5f3d2e67368664e2ad2b0
Goran <goran.schyns@student.hepl.be>
2021-08-19 23:36:31 +0200

    start questions

--- src/components/NewCatch/index.js
+++ src/components/NewCatch/index.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Questions} from "../Questions";
 import Logout from "../Logout";
-import {useHistory} from "react-router-dom";
 
-const NewCatch = () => {
+class NewCatch extends Component {
 
-    const history = useHistory();
+    state = {
+        levelNames: ["neutre"],
+        currentLevel: 0,
+        storedQuestions: [],
+        question: null,
+        type: null,
+        options: [],
+        idQuestion: 0
+    }
 
-    return (
-        <div>
-            <Logout/>
-            <h2>Signaler une capture</h2>
-            <p className="progression">Progression: 10%</p>
-            <div className="question">
-                <h2>Comment l'oiseau a-t-il été capturé ?</h2>
-                <p className="answer">Au nid</p>
-                <p className="answer">Avec filet</p>
-                <p className="answer">Autre</p>
-                <btn className="links" title="Question suivante">Suivant</btn>
+    loadQuestions = level => {
+        const fetchedArrayQuestions = Questions[0].quizz[level];
+
+        this.setState({
+            storedQuestions: fetchedArrayQuestions
+        })
+    }
+
+    componentDidMount() {
+        this.loadQuestions(this.state.levelNames[this.state.currentLevel]);
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(this.state.storedQuestions !== prevState.storedQuestions) {
+            this.setState({
+                question: this.state.storedQuestions[this.state.idQuestion].question,
+                type: this.state.storedQuestions[this.state.idQuestion].type,
+                options: this.state.storedQuestions[this.state.idQuestion].options
+            })
+        }
+    }
+
+    render() {
+
+        const displayOptions = this.state.options.map((option, index)=>{
+            return (
+                <p key={index} className="answer">{option}</p>
+            )
+        })
+
+        return (
+            <div>
+                <Logout/>
+                <h2 className="new-catch_title">Signaler une capture</h2>
+                <p className="progression">Progression: 10%</p>
+                <div className="question">
+                    <h2 className="question_title">{this.state.question}</h2>
+                    {displayOptions}
+                    <button className="links" title="Question suivante">Suivant</button>
+                </div>
             </div>
-            <a onClick={() => history.goBack()} className="links secondary" title="Retour à la page précédente">Retour</a>
-        </div>
-    )
+        )
+    }
 }
 
 
---
+++ src/components/Questions/index.js
@@ -0,0 +1,40 @@
+export const Questions = [
+    {
+        quizz: {
+            neutre:[
+                {
+                    id: 0,
+                    question: "Comment l'oiseau a-t-il été capturé ?",
+                    type: "options",
+                    options: [
+                        "Au nid",
+                        "Avec filet",
+                        "Autre"
+                    ],
+                    title: "catch_type"
+                },
+                {
+                    id: 1,
+                    question: "Quand a-t-il été capturé ?",
+                    type: "date",
+                    options: [],
+                    title: "date"
+                },
+                {
+                    id: 2,
+                    question: "Où l'oiseau a-t-il été capturé ?",
+                    type: "text",
+                    options: [],
+                    title: "location"
+                },
+                {
+                    id: 3,
+                    question: "Quel est le numéro de bague ?",
+                    type: "text",
+                    options: [],
+                    title: "catch_type"
+                }
+            ]
+        }
+    }
+]
